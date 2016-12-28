a33fa938a564c883857e69b3fe133274031bd698
Mukund Krishnamurthy <mukund.kri@gmail.com>
2016-12-28 20:26:26 +0530

    Adding basic webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path              = require('path');
+var webpack           = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+
+  entry: './src/script/coffee/application',
+  output: {
+    path: path.resolve('./build/webpack_bundles/'),
+    filename: '[name].js'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.coffee$/, loader: "coffee-loader" },
+      { test: /\.html$/, loader: "html-loader"},
+      { test: /\.hbs/, loader: "handlebars-template-loader" },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.coffee']
+  }
+
+}
